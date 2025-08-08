ce916ec51279e25d3dccec4857c1fd61a5c2a458
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2025-08-08 14:45:57 +0500

    ok

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import userRouter from './Routes/userRoute.js'
 import careerRouter from './Routes/careerRoute.js'
-import connectDB from './config/mongoDb.js'
+import connectDB from './config/mongoDb.js';
 import resumeRouter from "./Routes/resume.Route.js";
 import MockRouter from "./Routes/mockInterviewRoute.js";
 
