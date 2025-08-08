c426deb0a3538422e5dd14eba47f678369806e99
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2025-08-08 14:38:54 +0500

    ok

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ dotenv.config();
 import express from 'express'
 import cors from 'cors'
 import userRouter from './Routes/userRoute.js'
-import connectDB from './config/mongoDb.js'
 import careerRouter from './Routes/careerRoute.js'
+import connectDB from './config/mongoDb.js'
 import resumeRouter from "./Routes/resume.Route.js";
 import MockRouter from "./Routes/mockInterviewRoute.js";
 
